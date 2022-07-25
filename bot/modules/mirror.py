301e6b4a80530922853771383f0cb683d5efee1d
Wiszky <54537571+vishnoe115@users.noreply.github.com>
2022-07-26 04:11:10 +0700

    add edit msg

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -31,9 +31,10 @@ from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.mirror_utils.upload_utils.pyrogramEngine import TgUploader
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, delete_all_messages, update_all_messages
 from bot.helper.telegram_helper.button_build import ButtonMaker
 from bot.helper.ext_utils.db_handler import DbManger
+from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, delete_all_messages, update_all_messages, \
+                                                     deleteMessage, auto_delete_message, editMessage
 
 
 class MirrorListener:
