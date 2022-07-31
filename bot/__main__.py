6b6a8c449a5fe292f5e52b49d896ce4c6268b6cc
Wiszky <54537571+vishnoe115@users.noreply.github.com>
2022-07-31 10:05:03 +0700

    Replaced Emoji 249 Bot Restarted!

--- bot/__main__.py
+++ bot/__main__.py
@@ -280,6 +280,12 @@ def main():
             chat_id, msg_id = map(int, f)
         bot.edit_message_text("Restarted Successfully!", chat_id, msg_id)
         osremove(".restartmsg")
+    elif not notifier_dict and AUTHORIZED_CHATS:
+        for id_ in AUTHORIZED_CHATS:
+            try:
+                bot.sendMessage(id_, "😂", 'HTML')
+            except Exception as e:
+                LOGGER.error(e)
 
     start_handler = CommandHandler(BotCommands.StartCommand, start, run_async=True)
     ping_handler = CommandHandler(BotCommands.PingCommand, ping,
